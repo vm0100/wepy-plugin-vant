6d465acf72475a67eaab86a768a39082b556407d
Wei <vm0100@163.com>
2019-05-27 23:01:01 +0800

    修改target目录为动态获取

--- src/copy.js
+++ src/copy.js
@@ -2,24 +2,23 @@ import { existsSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import copydir from "copy-dir";
 
-const VANT_PATH = eval("require.resolve('vant-weapp/package.json').replace(/package.json$/, '')");
-const VANT_VERISON = eval("require('vant-weapp/package.json').version");
-
 import { VANT_SOURCE_DIR, VERSION_FILE_NAME } from "./config";
 
 // 复制Vant的文件到src中
 const copyVantToSrc = (componentsDir) => {
-  const sourcePath = join(VANT_PATH, VANT_SOURCE_DIR);
+  const van_path = eval("require.resolve('" + componentsDir + "/package.json').replace(/package.json$/, '')")
+  const van_verison = eval("require('" + componentsDir + "/package.json').version");
+  const sourcePath = join(van_path, VANT_SOURCE_DIR);
   const targetPath = join("src", "components", componentsDir);
   const versionPath = join("src", "components", componentsDir, VERSION_FILE_NAME);
 
   // 检测src目录下的副本
   if (existsSync(targetPath) && existsSync(versionPath)) {
     const copyVersion = readFileSync(versionPath, "utf-8");
-    if (copyVersion === VANT_VERISON) return; // 比对版本
+    if (copyVersion === van_verison) return; // 比对版本
   }
   copydir.sync(sourcePath, targetPath); // 复制文件夹
-  writeFileSync(versionPath, VANT_VERISON); // 添加版本文件
+  writeFileSync(versionPath, van_verison); // 添加版本文件
   addCopyFolderToGitIgnore(componentsDir); // 把复制过去的文件夹添加.gitignore
 };
 
